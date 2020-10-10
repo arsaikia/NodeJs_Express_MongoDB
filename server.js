9eaed36f36bdebcdc294e4a243588789eca447f0
Arunabh <arunabh.matrix@gmail.com>
2020-10-10 18:15:15 -0500

    SETUP AND CONNECT MONGO+COMPASS

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const logger = require("./middleware/logger");
 const morgan = require("morgan");
-
-// route Files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDB();
+
+// route Files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -21,7 +26,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandler promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close Server
+  server.close(() => process.exit(1));
+});
