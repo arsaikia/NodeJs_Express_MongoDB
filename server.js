85b447b658beefbcc4c6ed2372de6722bcb58e13
Arunabh <arunabh.matrix@gmail.com>
2020-10-10 16:29:20 -0500

    Define the requests

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamp", (req, res) => {
+  res.status(200).json({ success: true, msg: "Get All Bootcamps." });
+});
+
+app.get("/api/v1/bootcamp/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Get Bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamp", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create New Request" });
+});
+
+app.put("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
