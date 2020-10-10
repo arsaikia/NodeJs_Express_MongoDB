2a5316b854265861c287734f82d557aad255d6f5
Arunabh <arunabh.matrix@gmail.com>
2020-10-10 16:19:16 -0500

    Express Set Up

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
-const http = require("http");
+const express = require("express");
+const dotenv = require("dotenv");
 
-const todos = [
-  { id: 1, text: "Todo One" },
-  { id: 2, text: "Todo Two" },
-  { id: 3, text: "Todo Three" },
-];
+// Load env variables
+dotenv.config({ path: "./config/config.env" });
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("X-Powered-By", "Node.js");
+const app = express();
 
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todos,
-    })
-  );
-});
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+app.listen(
+  PORT,
+  console.log(`Server Running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+);
