e9f2564bf7bd3fd87a31b6a84878404fbb18a9e7
Arunabh <arunabh.matrix@gmail.com>
2020-10-10 17:05:30 -0500

    Dev logger using MORGAN

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 // route Files
 const bootcamps = require("./routes/bootcamps");
@@ -10,9 +11,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// Middleware
-
-app.use(logger);
+// Dev Logger Middleware
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
